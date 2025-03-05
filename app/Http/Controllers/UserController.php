ac8d27d5a5d31e1b1c87a08b0f2ea6ff095e3d7e
yehaoze <yehaozee@gmail.com>
2025-03-05 16:09:18 +0100

    2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Controllers\ResponseController;
+use App\Http\Requests\UserRequest;
 
-class UserController extends Controller
+class UserController extends ResponseController
 {
-    //
+    public function addUser(UserRequest $request) {
+
+        $user = new User();
+        $user->name = $request['name'];
+        $user->email = $request['email'];
+        $user->password = bcrypt($request['password']);
+        $user->phone_number = $request['phone_number'];
+        $user->festivals_id = $request['festivals_id'];
+        $user->save();
+
+        return $this->sendResponse($user,"Sikeres!");
+    }
+
+    // Felhasználók listázása
+    public function listUsers()
+    {
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+    // Egyedi felhasználó lekérése
+    public function getUser($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CityController;
 use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,7 @@ Route::post('/addCity',[CityController::class,'addCity']);
 Route::get('/getCities',[CityController::class,'getCities']);
 Route::post('/updateCity/{id}',[CityController::class,'updateCity']);
 Route::delete('/deleteCity/{id}',[CityController::class,'deleteCity']);
+
+Route::post('/adduser', [UserController::class, 'addUser']);
+Route::get('/users', [UserController::class, 'listUsers']);
+Route::get('/users/{id}', [UserController::class, 'getUser']);
